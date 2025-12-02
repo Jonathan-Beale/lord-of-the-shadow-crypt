0d19d3d11d53405cbc16457a2563ea72ab31bfb3
Tyler Orr <tyorrty@gmail.com>
2025-12-02 00:14:51 -0500

    please work

--- Scripts/hit_box.gd
+++ Scripts/hit_box.gd
@@ -4,7 +4,7 @@ extends Area2D
 @onready var collision_shape: CollisionShape2D = $"CollisionShape2D"
 @onready var user: Fighter = get_owner()
 
-var DAMAGE = 10
+var DAMAGE = 300
 var KNOCKBACK = 10
 var DAMAGE_TYPE = "physical"
 
